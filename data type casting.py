cab9d8427bd8c32d89d5103e4bdbf6540624bc3c
Yaswanth40 <154802555+Yaswanth40@users.noreply.github.com>
2024-01-17 09:34:41 +0530

    Add files via upload

---
+++ data type casting.py
@@ -0,0 +1,29 @@
+a=int(786) # 786
+b=int(6.33) # 6
+c=int("2023") # 2023
+d=int(90<100) # 1
+print(a,b,c,d)
+
+
+a=float(945) # 945.0
+b=float(87.33) # 87.33
+c=float("2024") # 2024.0
+d=float(3073 > 2178) # 1.0
+print(a,b,c,d)
+
+
+
+a=str(765) # 765
+b=str(657.383) # 657.383
+c=str("777") # 777
+d=str(29 < 19) # False
+print(a,b,c,d)
+
+
+
+
+a=bool(64326) # True
+b=bool(345.224) # True
+c=bool("47") # True
+d=bool(391 > 789) # True
+print(a,b,c,d)
---
+++ string slicing.py
@@ -0,0 +1,29 @@
+
+s = "January Month"
+print(s[0:3:1]) # Jan
+print(s[0:3]) # Jan
+print(s[2:6:1]) # nuar
+print(s[2:6]) # nuar
+print(s[3:7:1]) #uary
+print(s[3:7]) #uary
+print(s[5:6:1]) # r
+print(s[3:8:2]) # ur
+print(s[2:9:2]) # nayM
+print(s[:9:2]) # JnayM
+print(s[::1]) #  January Month
+print(s[::-1]) # hynoM yraunaJ
+print(s[9:3:-1]) # oM yra
+print(s[7:2:-2]) # ru
+print(s[8:3:-2]) # Mya
+print(s[7:4:-1]) # yr
+print(s[9:2:-3]) # oyu
+print(s[-5:-2]) # Mon
+print(s[-9:-3]) # ary Mo
+
+
+a = "andhra pradesh and telangana"
+print(a[-15:-22:-1])  #hsedarp
+print(a[7:14:])  #pradesh
+print(a[-15:-22])  #
+print(a[-22:-15])  # pradesh
+
