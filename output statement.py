8fac96c76069161617f500ec478c19afab2612c0
Yaswanth40 <154802555+Yaswanth40@users.noreply.github.com>
2024-01-17 14:47:48 +0530

    Add files via upload

---
+++ output statement.py
@@ -0,0 +1,18 @@
+
+print("january", end=" ")
+print("month")
+
+print("February", end="*")
+print("February")
+
+
+print("python is a \n case sensitive")
+
+print('python is a high level', 'programming language', sep='.')
+
+print('Python  is a ' + 'interpreted')
+
+a = 40
+b = 80
+
+print('The value of a is {} and b is {}'.format(a,b))
