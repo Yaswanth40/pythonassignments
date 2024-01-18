f1b3e8006baffca2f95dc2962a9779df30588c71
Yaswanth40 <154802555+Yaswanth40@users.noreply.github.com>
2024-01-18 15:11:18 +0530

    Add files via upload

---
+++ conditional statement.py
@@ -0,0 +1,32 @@
+name=input("enter mobile name to check availabity : ")
+
+if name=="samsung":
+    print(name.upper(),"yes it is available")
+    print(name.lower(),"yes it is available")
+elif name=="realme":
+    print(name.upper(),"yes it is available")
+    print(name.lower(),"yes it is available")
+elif name=="redmi":
+    print(name.upper(),"yes it is available")
+    print(name.lower(),"yes it is available")
+elif name=="iphone":
+    print(name.upper(),"yes it is available")
+    print(name.lower(),"yes it is available")
+elif name=="vivo":
+    print(name.upper(),"yes it is available")
+    print(name.lower(),"yes it is available")
+elif name=="oppo":
+    print(name.upper(),"yes it is available")
+    print(name.lower(),"yes it is available")
+elif name=="nothing":
+    print(name.upper(),"yes it is available")
+    print(name.lower(),"yes it is available")  
+elif name=="googlepixel":
+    print(name.upper(),"yes it is available")
+    print(name.lower(),"yes it is available")
+elif name=="motorola":
+    print(name.upper(),"yes it is available")
+    print(name.lower(),"yes it is available")
+else:
+    print(name,"sorry it is out of stock, please visit tomorrow again")
+    
