ef1bdf7904d751fab9d294c2128db0ba77a6db55
Yaswanth40 <154802555+Yaswanth40@users.noreply.github.com>
2024-01-23 14:57:40 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,18 @@
+
+
+my_username = "Yaswanth"
+my_password = "12345"
+
+while True:
+    username = input("Enter username:")
+    password = input("Enter password:")
+
+    if username == my_username and password == my_password:
+        print("Login succesfull.")
+    else:
+        print("your username  or password is incorrect. Try again!")
+
+
+
+
+
